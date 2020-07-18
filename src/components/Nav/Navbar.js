837e04070ad7338caa2f924e6eb422b5db2052ec
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-18 17:13:00 +0100

    i have lessened the padding in the navbar styled component, which is returned in the navbar component

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -1,31 +1,21 @@
-
 import React from 'react';
 import styled from 'styled-components';
 import Burger from './Burger';
 
 const Nav = styled.nav`
   z-index: 5;
-  padding: 0.7rem 0;
+  padding: 0.6rem 0;
   background-color: rgba(15, 119, 15, 0.6);
   display: flex;
   justify-content: space-between;
-  .logo {
-    font-size: 2.2rem;
-    color: white;
-    background: linear-gradient(135deg, #a1d5ff 0%,#ffffff 100%);
-    -webkit-text-fill-color: transparent;
-    -webkit-background-clip: text;
-    background-clip: text;
-  }
+  align-items: center;
 `
 
 const Navbar = () => {
   return (
     <Nav>
       <div className="header__container">
-        <div className="logo">
-          Adam Gardner
-      </div>
+        <h3 className="header__h3">Adam Gardner</h3>
         <Burger />
       </div>
     </Nav>
