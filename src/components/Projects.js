33f4bb36574ae2a753eed32a9aa7728332c34fa6
Adam Gardner <adam.gardner4991@gmail.com>
2020-07-30 18:11:03 +0100

    projects component updated, now contains two more projects (memovie and laravel vet site), date prop also added to each project stating the month and year it was made

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import ProjectsCard from './ProjectsCard';
-import football from '../assets/images/team-generator.png'
-import olympics from '../assets/images/lockdownolympics.png'
-import pingr from '../assets/images/pingrpic.png'
+import football from '../assets/images/team-generator.png';
+import olympics from '../assets/images/lockdownolympics.png';
+import pingr from '../assets/images/pingrpic.png';
+import memovie from '../assets/images/memovie.png';
+import vet from '../assets/images/laravets.png';
 
-const Projects = () => {
-    return (
-        <section className="projects" id="projects">
-            <h2 className="projects__h2">Projects</h2>
-            <div className="projects__container">
-                <ProjectsCard
-                    id="0"
-                    imgSrc={football}
-                    altText="Picture of Team Generator app"
-                    heading="Team Generator"
-                    technologies="React / Redux / Laravel / Heroku"
-                    description="A React/Redux football team picking app that that stores players and previous matches in a
-                    Laravel API. Users can edit players, change team settings and view match history."
-                    online={true}
-                    appHref="https://stormy-crag-38510.herokuapp.com/"
-                    repoHref="https://github.com/aejgardner/frontend-ui-five-aside"
-                />
-                <ProjectsCard 
+const Projects = () => (
+    <section className="projects" id="projects">
+        <h2 className="projects__h2">Projects</h2>
+        <div className="projects__container">
+            <ProjectsCard
+                id="0"
+                imgSrc={memovie}
+                altText="Picture of MeMovie app"
+                heading="MeMovie"
+                date="July 2020"
+                technologies="React / Redux / Immer / React Hooks"
+                description="A React/Redux movie app that allows users to pick a random movie from a table of movies they have entered, with or without filter requirements. Currently working on backend authentication with Laravel."
+                online={true}
+                appHref="https://aejgardner.github.io/memovie/#/"
+                repoHref="https://github.com/aejgardner/memovie"
+            />
+            <ProjectsCard
                 id="1"
+                imgSrc={football}
+                altText="Picture of Team Generator app"
+                heading="Team Generator"
+                date="June 2020"
+                technologies="React / Redux / Laravel / Heroku"
+                description="A React/Redux football team picking app that that stores players and previous matches in a
+                    Laravel API. Users can edit players, change team settings and view match history."
+                online={true}
+                appHref="https://stormy-crag-38510.herokuapp.com/"
+                repoHref="https://github.com/aejgardner/frontend-ui-five-aside"
+            />
+            <ProjectsCard
+                id="2"
                 imgSrc={olympics}
                 altText="Picture of Lockdown Olympics app"
                 heading="Lockdown Olympics"
+                date="June 2020"
                 technologies="React / Redux / Laravel / AWS"
                 description="Group project - a website for an alternative Olympics that uses a React/Redux front-end and
                 a
@@ -33,22 +48,34 @@ const Projects = () => {
                 online={true}
                 appHref="https://stormy-crag-38510.herokuapp.com/"
                 repoHref="https://github.com/aejgardner/lockdown-olympics"
-                />
-                <ProjectsCard 
-                id="2"
+            />
+            <ProjectsCard
+                id="3"
                 imgSrc={pingr}
                 altText="Picture of PINGR app"
                 heading="PINGR"
+                date="June/July 2020"
                 technologies="React / Redux"
                 description="A ping pong scoring app that enables the user to choose player names, serve alternation and
                 winning score. Additional logic included to determine deuce, game history and language
                 localisation to Esperanto. Built using React and Redux."
-                online={false}
+                online={true}
+                appHref="https://aejgardner.github.io/pingr/"
                 repoHref="https://github.com/aejgardner/ping-pong"
-                />
-            </div>
-        </section>
-    );
-};
+            />
+            <ProjectsCard
+                id="4"
+                imgSrc={vet}
+                altText="Picture of Vet app"
+                heading="Laravel Vet Project"
+                date="May 2020"
+                technologies="Laravel / Blade / MySql / Auth / TDD"
+                description="A mock veterinary practice database web application, developed over 2 weeks of the DevelopMe bootcamp. It uses Laravel Blade to display lists of owners and their animals from the database."
+                online={false}
+                repoHref="https://github.com/aejgardner/vet-project"
+            />
+        </div>
+    </section>
+);
 
 export default Projects;
