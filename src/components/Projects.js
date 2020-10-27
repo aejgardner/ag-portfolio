a0d4e5cd25574cd26d9001749e065cf3cb81b589
Adam Gardner <adam.gardner4991@gmail.com>
2020-10-27 22:08:59 +0000

    update cv and case studies

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ProjectsCard from './ProjectsCard';
 import football from '../assets/images/team-generator.png';
 import olympics from '../assets/images/lockdownolympics.png';
-import pingr from '../assets/images/pingrpic.png';
+import bagajob from '../assets/images/bagajob.png';
 import memovie_register from '../assets/images/memovie_register.png';
 import vet from '../assets/images/laravets.png';
 import photography from '../assets/images/photography.png';
@@ -11,8 +11,20 @@ const Projects = () => (
     <section className="projects" id="projects">
         <h2 className="projects__h2">Projects</h2>
         <div className="projects__container">
-            <ProjectsCard
+        <ProjectsCard
                 id="0"
+                imgSrc={bagajob}
+                altText="Picture of Bagajob app"
+                heading="Bagajob"
+                date="July/August 2020"
+                technologies="React / Redux / UI & UX Design"
+                description="I was the front end developer for an open source job management app built on behalf of the DevelopMe_ bootcamp. I built the front end using React and Redux, and  regularly liased with the UX Designer to ensure use of the app was intuitive and engaging."
+                online={true}
+                appHref="https://bagajob.developme.space/"
+                repoHref="https://github.com/develop-me/bagajob"
+            />
+            <ProjectsCard
+                id="1"
                 imgSrc={memovie_register}
                 altText="Picture of MeMovie app"
                 heading="MeMovie"
@@ -24,7 +36,7 @@ const Projects = () => (
                 repoHref="https://github.com/aejgardner/memovie"
             />
             <ProjectsCard
-                id="1"
+                id="2"
                 imgSrc={football}
                 altText="Picture of Team Generator app"
                 heading="Team Generator"
@@ -37,7 +49,7 @@ const Projects = () => (
                 repoHref="https://github.com/aejgardner/frontend-ui-five-aside"
             />
             <ProjectsCard
-                id="2"
+                id="3"
                 imgSrc={olympics}
                 altText="Picture of Lockdown Olympics app"
                 heading="Lockdown Olympics"
@@ -49,19 +61,6 @@ const Projects = () => (
                 online={true}
                 appHref="https://stormy-crag-38510.herokuapp.com/"
                 repoHref="https://github.com/aejgardner/lockdown-olympics"
-            />
-            <ProjectsCard
-                id="3"
-                imgSrc={pingr}
-                altText="Picture of PINGR app"
-                heading="PINGR"
-                date="June/July 2020"
-                technologies="React / Redux"
-                description="A ping pong scoring app that enables the user to choose player names, serve alternation and
-                winning score. Additional logic included to display game history and language
-                localisation to Esperanto. Built using React and Redux."
-                online={false}
-                repoHref="https://github.com/aejgardner/ping-pong"
             />
             <ProjectsCard
                 id="4"
