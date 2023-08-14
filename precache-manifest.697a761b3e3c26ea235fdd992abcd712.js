d2b678594a1629deb8343a86c8883d9640a1f1fe
Adam Gardner <adam.gardner4991@gmail.com>
2023-08-14 22:50:17 +0100

    Updates

---
+++ precache-manifest.697a761b3e3c26ea235fdd992abcd712.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3104bbe0339bd00be10327a6e41439",
+    "url": "/ag-portfolio/index.html"
+  },
+  {
+    "revision": "ee308ffc7c6e2b574d59",
+    "url": "/ag-portfolio/static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "4f7e842677441fa2e432",
+    "url": "/ag-portfolio/static/css/main.2a2fe079.chunk.css"
+  },
+  {
+    "revision": "ee308ffc7c6e2b574d59",
+    "url": "/ag-portfolio/static/js/2.d1a9d946.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/ag-portfolio/static/js/2.d1a9d946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7e842677441fa2e432",
+    "url": "/ag-portfolio/static/js/main.3a895013.chunk.js"
+  },
+  {
+    "revision": "66f48a07616ce6dff11f",
+    "url": "/ag-portfolio/static/js/runtime-main.25b0784e.js"
+  },
+  {
+    "revision": "9b8a6f94a5cb3ea46c9484ab47bf6598",
+    "url": "/ag-portfolio/static/media/adam_gardner_cv.9b8a6f94.pdf"
+  },
+  {
+    "revision": "7b754c422244d565e0c61f1e63d0e14e",
+    "url": "/ag-portfolio/static/media/bagajob.7b754c42.png"
+  },
+  {
+    "revision": "7a0bdde7480cacd8728a45f75241292b",
+    "url": "/ag-portfolio/static/media/laravets.7a0bdde7.png"
+  },
+  {
+    "revision": "252c8a2b4c9fc47e73f931282d27b8bd",
+    "url": "/ag-portfolio/static/media/lockdownolympics.252c8a2b.png"
+  },
+  {
+    "revision": "49ca61880d87c63475c58c9b0e7815ae",
+    "url": "/ag-portfolio/static/media/memovie_register.49ca6188.png"
+  },
+  {
+    "revision": "a6dfa18c23cfb8c04c1c2c98130d3cd2",
+    "url": "/ag-portfolio/static/media/photography.a6dfa18c.png"
+  },
+  {
+    "revision": "38dea53fbaf2594c67bd42e6bae1b381",
+    "url": "/ag-portfolio/static/media/portrait.38dea53f.jpg"
+  },
+  {
+    "revision": "d0a1142422f62765c47a75121c4734a6",
+    "url": "/ag-portfolio/static/media/team-generator.d0a11424.png"
+  }
+]);
